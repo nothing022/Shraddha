92393b2fea93b8d922ee9b197525f441d46f507d
root <root@vmi2018610.contaboserver.net>
2024-09-04 20:11:20 +0200

    Added refresh

--- ShraddhaMusic/plugins/tools/active.py
+++ ShraddhaMusic/plugins/tools/active.py
@@ -1,9 +1,10 @@
 from pyrogram import filters
-from pyrogram.types import Message
+from pyrogram.types import Message,InlineKeyboardMarkup,InlineKeyboardButton
 from unidecode import unidecode
 
 from ShraddhaMusic import app
 from ShraddhaMusic.misc import SUDOERS
+from ShraddhaMusic.utils.inline import close_markup
 from ShraddhaMusic.utils.database import (
     get_active_chats,
     get_active_video_chats,
@@ -13,8 +14,27 @@ from ShraddhaMusic.utils.database import (
 
 
 @app.on_message(filters.command(["activevc", "activevoice"]) & SUDOERS)
-async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
+async def activevccmd(client,message):
+   inline = False
+   await activevc(client,message,inline)
+
+@app.on_callback_query(filters.regex("refreshactivevc") & ~ SUDOERS)
+async def sysinfocmd(client, update):
+   await update.answer("❌ ONLY SUDO USERS ALLOWED TO REFRESH STATS❗", show_alert=True)
+   
+@app.on_callback_query(filters.regex("refreshactivevc") & SUDOERS)
+async def sysinfocmd(client, update):
+   inline = True
+   message = update.message
+   await activevc(client,message,inline)
+   await update.answer("🖥️ Chatlist updated❗")
+
+async def activevc(client,message,inline):
+    keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("⍟ ʀᴇꜰʀᴇꜱʜ ⍟",callback_data="refreshactivevc")],[InlineKeyboardButton("⍟ ᴄʟᴏꜱᴇ ⍟",callback_data="close")]])
+    if inline:
+      mystic = await message.edit_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...", reply_markup=keyboard)
+    else:
+      mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...", reply_markup=keyboard)
     served_chats = await get_active_chats()
     text = ""
     j = 0
@@ -36,12 +56,11 @@ async def activevc(_, message: Message):
         except:
             continue
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ {app.mention}.",reply_markup=keyboard)
     else:
         await mystic.edit_text(
             f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs :</b>\n\n{text}",
-            disable_web_page_preview=True,
-        )
+            disable_web_page_preview=True,reply_markup=keyboard)
 
 
 @app.on_message(filters.command(["activev", "activevideo"]) & SUDOERS)

7d2942b881f8b37246558738376ce757cec27b71
root <root@vmi2018610.contaboserver.net>
2024-09-08 06:09:00 +0200

    m cmd fix

--- ShraddhaMusic/plugins/bot/start.py
+++ ShraddhaMusic/plugins/bot/start.py
@@ -76,7 +76,7 @@ async def start_pm(client, message: Message, _):
                     chat_id=config.LOG_GROUP_ID,
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
-        elif name[0:5] == "help" if config.SEND_START_MESSAGE else "music":
+        if name[0:4] == ("help" if config.SEND_START_MESSAGE else "musi"):
           keyboard = help_pannel(_, True)
           UP, CPU, RAM, DISK = await bot_sys_stats()
           await message.reply_photo(
